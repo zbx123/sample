a20abd1855f1d32b1d07714e65dc9ce5ce1b10b6
zbx <1984170342@qq.com>
2018-04-16 02:36:53 +0000

    static pages

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	@extends('layouts.default')
+	@section('title','帮助')
+
+	@section('content')
+    <h1>帮助页</h1>
+    @stop
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>home</title>
+</head>
+<body>
+	@extends('layouts.default')
+	@section('content')
+<h1>主页</h1>
+@stop
+</body>
+</html>
