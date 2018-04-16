8cc4def9bd5a830c6f49b6f1f8e92d8e871c68d0
zbx <1984170342@qq.com>
2018-04-16 04:59:44 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>@yield('title','Sample') - laravel 新手入门教程</title>
-</head>
-<body>
-  @yield('content')
-</body>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <header class="navbar navbar-fixed-top navbar-inverse">
+    	@include('layouts._header')
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+        	@yield('content')
+        	@include('layouts._footer')
+          <a href="/" id="logo">Sample App</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/help">帮助</a></li>
+              <li><a href="#">登录</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <div class="container">
+      @yield('content')
+    </div>    
+  </body>
 </html>
